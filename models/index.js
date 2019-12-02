0e5c469e49707086eada048a8761452189d15c26
phuongdd3 <phuongdd3@vng.com.vn>
2019-12-02 16:19:19 +0700

    dev

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var db = {};
 const { db: { host, port, name, username, password} } = config;  
 // let connectionString = `mongodb+srv://${username}:${password}@${host}/${name}?retryWrites=true&w=majority`;
-let connectionString = `mongodb://${username}:${password}@${host}:${port}/?ssl=true&appName=@on-tap-cung-be@`;
+let connectionString = process.env.CUSTOMCONNSTR_MyConnectionString || `mongodb://${username}:${password}@${host}:${port}/?ssl=true&appName=@on-tap-cung-be@`;
 
 const options = {
     useNewUrlParser: true,
